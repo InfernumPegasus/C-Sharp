d05d5c2a2700747342967056fed27056277cf840
InfernumPegasus <vkirillov505@gmail.com>
2021-07-08 22:58:47 +0300

    Реализация полиморфизма

---
+++ Lessons/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace Lessons
+{
+    class Player
+    {
+        public void Reload(Weapon weapon)
+        {
+            weapon.Reload();
+        }
+        public void Fire(Weapon weapon)
+        {
+            weapon.Fire();
+        }
+
+        public void CheckInfo(Weapon weapon)
+        {
+            weapon.ShowInfo();
+        }
+    }
+
+    abstract class Weapon
+    {
+        public abstract int Damage { get; }
+        public abstract void Reload();
+        public abstract void Fire();
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Name: {GetType().Name}\nDamage: {Damage}");
+        }
+    }
+    class Bow : Weapon
+    {
+        public override int Damage { get { return 3; } }
+        public override void Reload()
+        {
+            Console.WriteLine("Reloading Bow!");
+        }
+        public override void Fire()
+        {
+            Console.WriteLine("Shoot!");
+        }
+    }
+
+    class LaserGun : Weapon
+    {
+        public override int Damage { get { return 10; } }
+        public override void Reload()
+        {
+            Console.WriteLine("Reloading Lasergun!");
+        }
+        public override void Fire()
+        {
+            Console.WriteLine("Laser shoot!");
+        }
+    }
+
+    class Crossbow : Weapon
+    {
+        public override int Damage => 5;
+        public override void Reload()
+        {
+            Console.WriteLine("Reloading crossbow!");
+        }
+        public override void Fire()
+        {
+            Console.WriteLine("Crossbow shoot");
+        }
+    }
+
+    class Rifle : Weapon
+    {
+        public override int Damage => 12;
+
+        public override void Fire()
+        {
+            Console.WriteLine("Rifle shoot!");
+        }
+
+        public override void Reload()
+        {
+            Console.WriteLine("Reloading rifle");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            
+            Weapon[] inventory = { new Bow(), new Crossbow(), new LaserGun(), new Rifle() };
+            foreach (var item in inventory)
+            {
+                player.CheckInfo(item);
+                player.Reload(item);
+                player.Fire(item);
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Lessons/User.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace User
+{
+    /// <summary>
+    /// Класс для корректного ввода чисел типа int или double из входного потока
+    /// </summary>
+    class Input
+    {
+        /// <summary>
+        /// Повторяет запрос на ввод числа типа int, пока то не будет удовлетворять требованиям этого типа
+        /// </summary>
+        /// <returns>
+        /// Возвращает значение типа int из входного потока
+        /// </returns>
+        public static int GetInt()
+        {
+            string str;
+            int num;
+            bool isSuccess;
+            do
+            {
+                Console.WriteLine("Введите корректное число:");
+                str = Console.ReadLine();
+                isSuccess = int.TryParse(str, out num);
+            } while (!isSuccess || num > int.MaxValue || num < int.MinValue);
+
+            return num;
+        }
+
+        /// <summary>
+        /// Повторяет запрос на ввод числа типа double, пока то не будет удовлетворять требованиям этого типа
+        /// </summary>
+        /// <returns>
+        /// Возвращает значение типа double из входного потока
+        /// </returns>
+        public static double GetDouble()
+        {
+            string str;
+            double num;
+            bool isSuccess;
+
+            do
+            {
+                Console.WriteLine("Введите корректное число:");
+                str = Console.ReadLine();
+                isSuccess = double.TryParse(str, out num);
+            } while (!isSuccess || num > double.MaxValue || num < double.MinValue);
+
+            return num;
+        }
+    }
+}
