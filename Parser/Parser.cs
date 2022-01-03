c09590c4e016fa07db47608a1df6199a8dab124d
InfernumPegasus <vkirillov505@gmail.com>
2022-01-03 16:35:53 +0300

    Simple file parser

---
+++ Parser/Parser.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace Parser;
+
+internal static class Parser
+{
+    public static IEnumerable<string> ReadTextLines(string fileName, Encoding encoding)
+    {
+        IEnumerable<string> textLines;
+
+        if (!File.Exists(fileName))
+            throw new FileNotFoundException($"[Occured in {nameof(ReadTextLines)}] No such file!");
+            
+        try
+        {
+            textLines = File.ReadLines(fileName, encoding);
+            textLines = textLines.Where(x => x != "");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new DirectoryNotFoundException($"[Occured in {nameof(ReadTextLines)}] Wrong filename!");
+        }
+
+        return textLines;
+    }
+
+    public static void ShowTextLines(IEnumerable<string> textLines)
+    {
+        try
+        {
+            var enumerable = textLines.ToList();
+            Console.WriteLine($"Strings amount : {enumerable.Count()}");
+            foreach (var textLine in enumerable)
+                Console.WriteLine($"{textLine}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw new Exception($"[Occured in {nameof(ShowTextLines)}] No info in file!");
+        }
+    }
+}
---
+++ Parser/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Parser
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            const string fileName = "../../../TestFile.txt";
+            var encoding = Encoding.UTF8;
+            
+            try
+            {
+                var textLines = Parser.ReadTextLines(fileName: fileName, encoding: encoding);
+                Parser.ShowTextLines(textLines: textLines);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
